82ee737568a6e8d5c4c9d1d57db28f30f30d351e
Henrique Molinari <henrique.molinari@sou.unifeob.edu.br>
2026-08-17 22:19:19 -0300

    Commit final

--- Arquivos csv/script.py
+++ Arquivos csv/script.py
@@ -1,9 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-engine = create_engine(
-    "sqlite:///aula17082026a.db"
-)
+engine = create_engine("sqlite:///aula17082026a.db")
 
 arquivos = ["NCM.csv", "produtos.csv", "vendas.csv"]
 
@@ -11,17 +9,9 @@ for arquivo in arquivos:
 
     tabela = arquivo.replace(".csv", "")
 
-    df = pd.read_csv(
-        arquivo,
-        sep=";",
-        encoding="latin1"
-    )
+    df = pd.read_csv(arquivo, sep=";", encoding="latin1")
 
-    df.to_sql(
-        tabela,
-        engine,
-        if_exists="replace",
-        index=False
-    )
+    df.to_sql(tabela, engine, if_exists="replace", index=False)
 
     print(f"{tabela} importada")
+  
